c222b48d0444bca6f537f8f02bb00b8540055bcd
MaxEfimov <efimov@mail.ru>
2025-12-05 21:08:35 +0300

    Добавьте файлы проекта.

---
+++ CppDataTypesProject/CppDataTypesProject.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+int main()
+{
+    /*int n;
+
+    std::cout << "bool: " << sizeof(bool) << "\n";
+    std::cout << "char: " << sizeof(char) << "\n";
+    std::cout << "short: " << sizeof(short) << "\n";
+    std::cout << "int: " << sizeof(n) << "\n";
+    std::cout << "long long: " << sizeof(long long) << "\n";
+
+    char str[] = "Hello";*/
+
+    //std::cout << str << "\n";
+
+
+    /*int a{ 50 }, b{ 22 };
+
+    std::cout << a << " + " << b << " = " << a + b << "\n";
+    std::cout << a << " - " << b << " = " << a - b << "\n";
+    std::cout << a << " * " << b << " = " << a * b << "\n";
+    std::cout << a << " / " << b << " = " << (float)a / b << "\n";
+    std::cout << a << " % " << b << " = " << a % b << "\n";*/
+
+    /*int ticket;
+    std::cout << "Input number of ticket: ";
+    std::cin >> ticket;
+
+    int d1{ ticket / 100000 };
+    int d2{ (ticket / 10000) % 10 };
+
+    int d3{ (ticket / 1000) % 10 };
+    int d4{ (ticket / 100) % 10 };
+    int d5{ (ticket / 10) % 10 };
+    int d6{ ticket % 10 };
+
+    int sum1 = d1 + d2 + d3;
+    int sum2 = d4 + d5 + d6;
+
+    std::cout << sum1 - sum2 << "\n";*/
+
+    int n{ 5 };
+
+    int m = n++ + n++;
+
+    // n++; // postfix
+    // ++n; // prefix
+    /*int r = n++;
+    std::cout << n << "\n";
+    std::cout << r << "\n";*/
+}
